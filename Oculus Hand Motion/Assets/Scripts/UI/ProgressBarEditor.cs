b8bc1abfaa7832df9e5f9e625c7ccfb3e1c2c73e
rlatkddn83 <84095116+rlatkddn83@users.noreply.github.com>
2022-10-14 17:49:03 +0900

    update 10/14

--- Oculus Hand Motion/Assets/Scripts/UI/ProgressBarEditor.cs
+++ Oculus Hand Motion/Assets/Scripts/UI/ProgressBarEditor.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR_WIN
+using UnityEditor;
 
 [CustomEditor(typeof(ProgressBar))]
 public class ProgressBarEditor : Editor
@@ -22,3 +23,4 @@ public class ProgressBarEditor : Editor
 
     
 }
+#endif
